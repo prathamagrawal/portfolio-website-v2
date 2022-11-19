a7ccd159aabfdebd87be014054f67b368f9b3dfd
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-11-20 01:11:42 +0530

    Added Hero page

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -70,13 +70,8 @@ const Hero = () => {
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Check out my course!
+    <a className="email-link" href="https://www.prathamagrawal.me" target="_blank" rel="noreferrer">
+      Check out my resume
     </a>
   );
 
