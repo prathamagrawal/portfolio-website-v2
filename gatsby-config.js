853d76ef0fe10378639ba9a64fae57fc5820bc90
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-11-20 01:15:43 +0530

    Updated Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Pratham Agrawal',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Brittany Chiang is a undergraduate student studying Computer Science and has a deep love for Data Science.',
+    siteUrl: 'https://www.prathamagrawal.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@fearsomejockey',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Pratham Agrawal',
+        short_name: 'Pratham Agrawal',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
