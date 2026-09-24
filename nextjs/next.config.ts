3399bc848d1e6ca69be3edbe80fa2312beaa5983
prathamagrawal-affinsys <prathamagrawal@affinsys.com>
2026-09-24 15:29:14 +0530

    chore: updated  nextjs routes ci:true

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -4,6 +4,45 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/resume.pdf",
+        permanent: false,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/pratham-manish-agrawal",
+        permanent: false,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/prathamagrawal",
+        permanent: false,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/fearsomejockey",
+        permanent: false,
+      },
+      {
+        source: "/kaggle",
+        destination: "https://www.kaggle.com/fearsomejockey",
+        permanent: false,
+      },
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/prathamonthemove/",
+        permanent: false,
+      },
+      {
+        source: "/email",
+        destination: "mailto:prathamagrawal1205@gmail.com",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
